8d0cefb3f263ecaa0859bc060f1db133e56c2c8a
gotovsky <skygsn@gmail.com>
2018-04-05 00:19:19 +0300

    Add storage concept

---
+++ src/Skygsn/Sitemap/Storage/MemoryStorage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Skygsn\Sitemap\Storage;
+
+class MemoryStorage implements Storage
+{
+    /**
+     * @var resource
+     */
+    private $resource;
+
+    /**
+     * @param string $name
+     * @param string $content
+     */
+    public function save(string $name, string $content)
+    {
+        $this->resource = fopen('php://temp', 'r+');
+        fwrite($this->resource, $content);
+        rewind($this->resource);
+    }
+
+    /**
+     * @return void
+     */
+    public function clean()
+    {
+        fclose($this->resource);
+    }
+
+    /**
+     * @return resource
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+}
