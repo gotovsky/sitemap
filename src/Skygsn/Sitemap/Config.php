1e884fb7bed321f30946b40482a4cee9dfeee19d
gotovsky <skygsn@gmail.com>
2018-04-06 01:11:12 +0300

    Update config

--- src/Skygsn/Sitemap/Config.php
+++ src/Skygsn/Sitemap/Config.php
@@ -3,6 +3,8 @@
 namespace Skygsn\Sitemap;
 
 use Skygsn\Sitemap\Exception\ConfigException;
+use Skygsn\Sitemap\Validator\SitemapValidator;
+use Skygsn\Sitemap\Validator\UrlValidator;
 
 class Config
 {
@@ -12,6 +14,29 @@ class Config
      * @var int
      */
     private $urlsPerSitemapLimit = self::DEFAULT_URLS_PER_SITEMAP_LIMIT;
+
+    /**
+     * @var UrlValidator[]
+     */
+    private $urlValidators = [];
+
+    /**
+     * @var SitemapValidator[]
+     */
+    private $sitemapValidators = [];
+
+    /**
+     * @var string
+     */
+    private $siteUrl;
+
+    /**
+     * @param string $siteUrl
+     */
+    public function __construct(string $siteUrl)
+    {
+        $this->siteUrl = $siteUrl;
+    }
 
     /**
      * @param int $urlsPerSitemapLimit
@@ -33,5 +58,45 @@ class Config
     public function getUrlsPerSitemapLimit(): int
     {
         return $this->urlsPerSitemapLimit;
+    }
+
+    /**
+     * @return UrlValidator[]
+     */
+    public function getUrlValidators(): array
+    {
+        return $this->urlValidators;
+    }
+
+    /**
+     * @return SitemapValidator[]
+     */
+    public function getSitemapValidators(): array
+    {
+        return $this->sitemapValidators;
+    }
+
+    /**
+     * @param SitemapValidator $sitemapValidator
+     */
+    public function addSitemapValidator(SitemapValidator $sitemapValidator)
+    {
+        $this->sitemapValidators[] = $sitemapValidator;
+    }
+
+    /**
+     * @param UrlValidator $urlValidator
+     */
+    public function addUrlValidator(UrlValidator $urlValidator)
+    {
+        $this->urlValidators[] = $urlValidator;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSitemapBaseUrl(): string
+    {
+        return $this->siteUrl;
     }
 }
